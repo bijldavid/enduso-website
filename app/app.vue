ef2db1c96da32b9ecf17d60ffc949bcae0f14858
David Bijl <davidbijl321@gmail.com>
2026-07-24 15:14:57 +0200

    favicon fix

--- app/app.vue
+++ app/app.vue
@@ -1,9 +1,21 @@
 <template>
-  <NuxtLayout />
+    <NuxtLayout />
 </template>
 
 <script setup>
 import '~/assets/css/reset.css'
 import '~/assets/css/general.css'
 import '~/assets/css/fonts.css'
+
+const { baseURL, cdnURL } = useRuntimeConfig().app
+
+useHead({
+    link: [
+        {
+            rel: 'icon',
+            type: 'image/x-icon',
+            href: `${cdnURL || baseURL}favicon.ico`,
+        },
+    ],
+})
 </script>
